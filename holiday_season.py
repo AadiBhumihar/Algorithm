e8668f02417a54907b51330a98853c1ac0f112fc
aadibhumihar <201451032@iiitvadodara.ac.in>
2018-01-06 01:26:07 +0530

    Algo Code

---
+++ holiday_season.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  6 01:17:28 2018
+
+@author: bhumihar
+"""
+
+''' 
+   Sample Input 
+'''
+   
+
+test_case_len = ('5')
+
+test_case = ('ababa')
+
+''' 
+   Code
+'''
+  
+ 
